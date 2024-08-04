b803900a53ef449e3a404cc2ed52c4c6351211fa
sideshowbobgot <sideshowbobgot@gmail.com>
2024-08-04 16:34:50 +0300

    added pawn attacks

--- src/checkers_data.cpp
+++ src/checkers_data.cpp
@@ -11,7 +11,6 @@
 #include <range/v3/view/transform.hpp>
 
 namespace mcts_checkers {
-
     static const auto DEFAULT_PLAYER_INDEX = std::bitset<CHEKCERS_CELLS_COUNT>{
         "00000"
         "00000"
@@ -57,72 +56,131 @@ namespace mcts_checkers {
         m_is_king{DEFAULT_IS_KING}
     {}
 
-    std::pair<std::vector<AttackAction>, uint64_t> collect_king_attacks(const CheckersData& data, Vector<uint8_t> checker_board_vector) {
-        return collect_king_attacks(data, convert_board_vector_to_checker_index(checker_board_vector));
+    template<bool is_king>
+    static std::pair<std::vector<AttackAction>, uint64_t> collect_attacks(const CheckersData& data, uint8_t checker_index);
+
+    template<bool is_king>
+    static auto block_traversal_general(
+        const int8_t block_x, const int8_t block_y,
+        const CheckersData& data,
+        const bool checker_player,
+        const uint8_t checker_index,
+        const uint8_t enemy_checker_index,
+        std::vector<AttackAction>& actions,
+        std::vector<uint64_t>& action_sizes
+    ) {
+        const auto block_board_vector = Vector{static_cast<uint8_t>(block_x), static_cast<uint8_t>(block_y)};
+        const auto block_checker_index = convert_board_vector_to_checker_index(block_board_vector);
+
+        if(data.m_is_in_place[block_checker_index]) {
+            if constexpr (is_king) {
+                return true;
+            } else {
+                return;
+            }
+        }
+
+        auto& attack_action = actions.emplace_back(convert_checker_index_to_board_index(block_checker_index));
+
+        auto new_data = data;
+        new_data.m_is_in_place.set(enemy_checker_index, false);
+        new_data.m_is_in_place.set(checker_index, false);
+        new_data.m_is_in_place.set(block_checker_index, true);
+        new_data.m_player_index.set(block_checker_index, checker_player);
+        new_data.m_is_king.set(block_checker_index, data.m_is_king[checker_index]);
+
+        auto [action, action_size] = collect_attacks<is_king>(new_data, block_checker_index);
+        action_sizes.emplace_back(action_size + 1);
+        attack_action.m_child_actions = std::move(action);
+
+        if constexpr (is_king) {
+            return false;
+        }
     }
 
-    std::pair<std::vector<AttackAction>, uint64_t> collect_king_attacks(const CheckersData& data, const uint8_t checker_index) {
-        constexpr auto deviations = std::array{
-            std::array<int8_t, 2>{-1, -1},
-            std::array<int8_t, 2>{CELLS_PER_SIDE, +1}
+    template<bool is_king>
+    static void enemy_traversal(
+        const int8_t x, const int8_t y,
+        const int8_t dev_x, const int8_t dev_y,
+        const int8_t bound_x, const int8_t bound_y,
+        const CheckersData& data,
+        const bool checker_player,
+        const uint8_t checker_index,
+        std::vector<AttackAction>& actions,
+        std::vector<uint64_t>& action_sizes
+    ) {
+        const auto enemy_board_index = Vector{
+            static_cast<uint8_t>(x),
+            static_cast<uint8_t>(y)
         };
+        const auto enemy_checker_index = convert_board_vector_to_checker_index(enemy_board_index);
+        if(
+            data.m_is_in_place[enemy_checker_index]
+            and data.m_player_index[enemy_checker_index] != checker_player
+        ) {
+            auto block_y = static_cast<int8_t>(y + dev_y);
+            auto block_x = static_cast<int8_t>(x + dev_x);
+
+            if constexpr (is_king) {
+                while(block_y != bound_y and block_x != bound_x) {
+                    const auto is_blocked = block_traversal_general<true>(
+                        block_x, block_y, data, checker_player, checker_index,
+                        enemy_checker_index, actions, action_sizes
+                    );
+                    if(is_blocked) break;
+
+                    block_y = static_cast<int8_t>(block_y + dev_y);
+                    block_x = static_cast<int8_t>(block_x + dev_x);
+                }
+            } else {
+                if(block_y != bound_y and block_x != bound_x) {
+                    block_traversal_general<false>(
+                        block_x, block_y, data, checker_player, checker_index,
+                        enemy_checker_index, actions, action_sizes
+                    );
+                }
+            }
+        }
+    }
+
+    static constexpr auto DEVIATIONS = std::array{
+        std::array<int8_t, 2>{-1, -1},
+        std::array<int8_t, 2>{CELLS_PER_SIDE, +1}
+    };
+
+    template<bool is_king>
+    static std::pair<std::vector<AttackAction>, uint64_t> collect_attacks(
+        const CheckersData& data,
+        const uint8_t checker_index
+    ) {
         auto actions = std::vector<AttackAction>{};
         auto action_sizes = std::vector<uint64_t>{};
         const auto checker_player = data.m_player_index[checker_index];
         const auto checker_vector = convert_checker_index_to_board_vector(checker_index);
-        for(const auto [bound_y, dev_y] : deviations) {
-            for(const auto [bound_x, dev_x] : deviations) {
+        for(const auto [bound_y, dev_y] : DEVIATIONS) {
+            for(const auto [bound_x, dev_x] : DEVIATIONS) {
                 auto y = static_cast<int8_t>(static_cast<int8_t>(checker_vector.y) + dev_y);
                 auto x = static_cast<int8_t>(static_cast<int8_t>(checker_vector.x) + dev_x);
-                while(y != bound_y and x != bound_x) {
-                    const auto enemy_board_index = Vector{
-                        static_cast<uint8_t>(x),
-                        static_cast<uint8_t>(y)
-                    };
-                    const auto enemy_checker_index = convert_board_vector_to_checker_index(enemy_board_index);
-                    if(
-                        data.m_is_in_place[enemy_checker_index]
-                        and data.m_player_index[enemy_checker_index] != checker_player
-                    ) {
-                        auto block_y = static_cast<int8_t>(y + dev_y);
-                        auto block_x = static_cast<int8_t>(x + dev_x);
 
-                        while(block_y != bound_y and block_x != bound_x) {
+                if constexpr(is_king) {
+                    while(y != bound_y and x != bound_x) {
+                        enemy_traversal<is_king>(
+                            x, y, dev_x, dev_y, bound_x, bound_y,
+                            data, checker_player, checker_index,
+                            actions, action_sizes
+                        );
 
-                            const auto block_board_vector = Vector{
-                                static_cast<uint8_t>(block_x),
-                                static_cast<uint8_t>(block_y)
-                            };
-
-                            const auto block_checker_index = convert_board_vector_to_checker_index(block_board_vector);
-
-                            if(data.m_is_in_place[block_checker_index]) {
-                                break;
-                            }
-
-                            auto& attack_action = actions.emplace_back(convert_checker_index_to_board_index(block_checker_index));
-
-                            auto new_data = data;
-                            new_data.m_is_in_place.set(enemy_checker_index, false);
-                            new_data.m_is_in_place.set(checker_index, false);
-
-                            new_data.m_is_in_place.set(block_checker_index, true);
-                            new_data.m_player_index.set(block_checker_index, checker_player);
-                            new_data.m_is_king.set(block_checker_index, true);
-
-                            {
-                                auto [action, action_size] = collect_king_attacks(new_data, block_checker_index);
-                                action_sizes.emplace_back(action_size + 1);
-                                attack_action.m_child_actions = std::move(action);
-                            }
-
-                            block_y = static_cast<int8_t>(block_y + dev_y);
-                            block_x = static_cast<int8_t>(block_x + dev_x);
-                        }
-                        break;
+                        y = static_cast<int8_t>(y + dev_y);
+                        x = static_cast<int8_t>(x + dev_x);
                     }
-                    y = static_cast<int8_t>(y + dev_y);
-                    x = static_cast<int8_t>(x + dev_x);
+                } else {
+                    if(y != bound_y and x != bound_x) {
+                        enemy_traversal<is_king>(
+                            x, y, dev_x, dev_y, bound_x, bound_y,
+                            data, checker_player, checker_index,
+                            actions, action_sizes
+                        );
+                    }
                 }
             }
         }
@@ -138,6 +196,19 @@ namespace mcts_checkers {
             | ranges::to_vector;
 
         return {utils::checked_move(range), max_action_size};
+    }
+
+    std::pair<std::vector<AttackAction>, uint64_t> collect_pawn_attacks(const CheckersData& data, const Vector<uint8_t> checker_board_vector) {
+        return collect_pawn_attacks(data, convert_board_vector_to_checker_index(checker_board_vector));
+    }
+    std::pair<std::vector<AttackAction>, uint64_t> collect_pawn_attacks(const CheckersData& data, const uint8_t checker_index) {
+        return collect_attacks<false>(data, checker_index);
+    }
+    std::pair<std::vector<AttackAction>, uint64_t> collect_king_attacks(const CheckersData& data, Vector<uint8_t> checker_board_vector) {
+        return collect_king_attacks(data, convert_board_vector_to_checker_index(checker_board_vector));
+    }
+    std::pair<std::vector<AttackAction>, uint64_t> collect_king_attacks(const CheckersData& data, uint8_t checker_index) {
+        return collect_attacks<true>(data, checker_index);
     }
 
 }
